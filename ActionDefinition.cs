230f7387e2ee073f6888b05269fc0ded680acd65
Arpan-18 <mondalarpan24052004@gmail.com>
2025-07-18 00:43:33 +0530

    Delete ActionDefinition.cs

--- ActionDefinition.cs
+++
@@ -1,9 +0,0 @@
-namespace WorkflowEngine.Models;
-
-public class ActionDef
-{
-    public string Id { get; set; } = "";
-    public bool Enabled { get; set; } = true;
-    public List<string> From { get; set; } = new();
-    public string To { get; set; } = "";
-}
